a1d32b70d25877f5a14fe90c103f97998f3f3963
blindcat <839884316@qq.com>
2017-01-19 02:34:36 +0800

    （前端）删除练习文件，（前端）新建前端页面基本样式、导航条

---
+++ src/client/src/components/Topbar.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <div id="logo_box">
+      <img src="../assets/blindcat.jpg">
+    </div>
+    <!-- 为根路由导航设置exact属性true -->
+    <ul id="topbar">
+      <li v-for="topbar_item in topbar_data">
+        <router-link 
+          :to="{name: topbar_item.routerName}" 
+          :class="$route.name" 
+          :exact="topbar_item.routerName == 'home'? true : false"
+        >
+          {{ topbar_item.name }}
+        </router-link>
+      </li>
+      <div class="clear"></div>
+    </ul>
+  </div>
+</template>
+
+<script type="text/javascript">
+  import { mapState } from 'Vuex'
+  export default {
+    name: 'Topbar',
+    computed: mapState({
+      topbar_data: state => state.topbar_data
+    })
+  }
+</script>
+
+<!-- 添加 "scoped" 属性使该css样式仅作用于该组件 -->
+<style scoped>
+  .clear {
+    clear: both;
+  }
+
+  #logo_box{
+    width: 100%;
+    display: -webkit-box;      /* OLD - iOS 6-, Safari 3.1-6 */
+    display: -moz-box;         /* OLD - Firefox 19- (buggy but mostly works) */
+    display: -ms-flexbox;      /* TWEENER - IE 10 */
+    display: -webkit-flex;     /* NEW - Chrome */
+    display: flex;             /* NEW, Spec - Opera 12.1, Firefox 20+ */
+    flex-wrap: nowrap;
+    justify-content: center;
+  }
+
+  #logo_box img{
+    width: 150px;
+    height: 150px;
+  }
+
+  #topbar {
+    width: 100%;
+    display: -webkit-box;      /* OLD - iOS 6-, Safari 3.1-6 */
+    display: -moz-box;         /* OLD - Firefox 19- (buggy but mostly works) */
+    display: -ms-flexbox;      /* TWEENER - IE 10 */
+    display: -webkit-flex;     /* NEW - Chrome */
+    display: flex;             /* NEW, Spec - Opera 12.1, Firefox 20+ */
+    flex-wrap: nowrap;
+    justify-content: center;
+
+    border-bottom: 1px solid #ccc;
+  }
+
+  #topbar li {
+    list-style: none;
+    height: 40px;
+    width: 80px;
+    -webkit-flex: 0 1 1;
+    flex: 0 1 1;
+    flex-grow : 0;
+    flex-shrink : 1;
+    flex-basis : 1;
+  }
+
+  #topbar li a {
+    display: block;
+    width: 100%;
+    height: 100%;
+    line-height: 40px;
+    text-align: center;
+    color: #333;
+    font-size: 16px;
+    font-weight: 600;
+    text-decoration: none;
+  }
+
+  #topbar li a.router-link-active {
+    color: #41b883;
+    font-size: 18px;
+    font-weight: 700;
+  }
+
+  #topbar li a:hover {
+    text-decoration: none;
+    color: #41b883;
+  }
+</style>
--- src/client/src/main.js
+++ src/client/src/main.js
@@ -1,24 +1,103 @@
 import Vue from 'vue'
+import Vuex from 'Vuex'
+
 import App from './App'
-import home from './components/home.vue'
+import Topbar from './components/Topbar.vue'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
 
-// 注册两个插件
+// 注册插件
 Vue.use(VueResource)
 Vue.use(VueRouter)
+Vue.use(Vuex)
 
-var routes = [
-  { path: '/home', component: home }
-]
+// 路由
+var routes = [{
+  path: '/',
+  exact: true,
+  name: 'home',
+  components: {
+    Topbar
+  }
+}, {
+  path: '/tags',
+  name: 'tags',
+  components: {
+    Topbar
+  }
+}, {
+  path: '/archive',
+  name: 'archive',
+  components: {
+    Topbar
+  }
+}, {
+  path: '/resources',
+  name: 'resources',
+  components: {
+    Topbar
+  }
+}, {
+  path: '/search',
+  name: 'search',
+  components: {
+    Topbar
+  }
+}, {
+  path: '/about',
+  name: 'about',
+  components: {
+    Topbar
+  }
+}]
+
 var router = new VueRouter({
   routes,
   mode: 'history'
 })
 
+// 全局Vuex
+const store = new Vuex.Store({
+  strict: true,
+  state: {
+    // 导航条数据
+    topbar_data: [{
+      name: '首页',
+      url: '',
+      routerName: 'home'
+    }, {
+      name: '分类',
+      url: 'tags',
+      routerName: 'tags'
+    }, {
+      name: '归档',
+      url: 'archive',
+      routerName: 'archive'
+    }, {
+      name: '资源',
+      url: 'resources',
+      routerName: 'resources'
+    }, {
+      name: '搜索',
+      url: 'search',
+      routerName: 'search'
+    }, {
+      name: '关于',
+      url: 'about',
+      routerName: 'about'
+    }]
+  },
+  mutations: {
+    addCount (state) {
+      state.count++
+    }
+  }
+})
+
 /* eslint-disable no-new */
 new Vue({
   router,
+  store,
   template: '<App/>',
   components: { App }
 }).$mount('#app')
