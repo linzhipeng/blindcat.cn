923615962e4396de08c19f78fe217054ac4b0805
blindcat <839884316@qq.com>
2017-01-24 01:01:18 +0800

    更新Readme.md，（后端）设置静态文件目录，（后端）去除js行末分号

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # 瞎猫的Node + Vue单页应用开发
-**进度：5%**
+**进度：10%**
 
-## 演示地址
-[http://vue.blindcat.cn](http://vue.blindcat.cn)
+## 前端演示地址
+[http://blindcat.cn](http://blindcat.cn)
+## 后端演示地址
+[http://api.blindcat.cn](http://api.blindcat.cn)
 
 ## 前端安装
 
@@ -15,4 +17,17 @@ npm install
 
 # 使用webpack打包build
 npm run build
+```
+
+## 后端安装
+
+``` bash
+# 进入后端代码目录
+cd ./src/server
+
+# 安装依赖
+npm install
+
+# 运行
+node app
 ```
--- src/server/app.js
+++ src/server/app.js
@@ -1,25 +1,30 @@
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
-var config = require('./config/config.js');
+var express = require('express')
+var path = require('path')
+var bodyParser = require('body-parser')
+var config = require('./config/config.js')
 
-var app = express();
-var router = express.Router();
+// 引入mongoose模块配置并连接数据库
+// var mongoose = require('./config/mongoose.js')
+// mongoose()
+
+var app = express()
+var router = express.Router()
 
 // 路由转发
-var index = require('./routers/index.server.router');
+var index = require('./routers/index.server.router')
 
 // 使用中间件
-// app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/static', express.static(__dirname + '/public')) // 指定public为静态文件目录
+// bodyParser设置解析http请求体
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // 监听请求并转发
-app.use('/', index);
+app.use('/', index)
 
 // 监听3000端口
 app.listen(config.port, function () {
-	console.log('listening on port '+ config.port +'!');
-});
+	console.log('listening on port '+ config.port +'!')
+})
 
-module.exports = app;
+module.exports = app
