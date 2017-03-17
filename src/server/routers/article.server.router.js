da7d2c5e8522dbbba547b69c1b1401e5321e0054
blindcat <839884316@qq.com>
2017-03-17 13:21:15 +0800

    （后端）文章列表查询增加页数、文章数、tags筛选

--- src/server/routers/article.server.router.js
+++ src/server/routers/article.server.router.js
@@ -15,20 +15,31 @@ router.use(bodyParser.urlencoded({ extended: true }))
 var data
 router
 	.get('/', function(req, res){
+		// 按照标签搜索
+		let searchType = {};
+		if (req.query.types) {
+			searchType = {'tags': {$all: [req.query.types]}}
+		}
+		// 对象解构赋值
+		// pageNum  —— 第几页（默认1）
+		// articleNum  —— 每页规定文章数（默认10）
+		let {pageNum = 1, articleNum = 10} = req.query
+		// 转为整型、正整数
+		pageNum = (parseInt(pageNum) > 0) ? parseInt(pageNum) : 1
+		articleNum = (parseInt(articleNum) > 0) ? parseInt(articleNum) : 10
+		
 		article
-			// .find(['title', 'content', 'abstract'])
-			.find({'tags': {$all: ['不错']}})
-			// .skip(1)
-			// .limit(10)
+			.find(searchType)
+			.skip((pageNum - 1) * articleNum)
+			.limit(articleNum)
 			.select('_id title abstract publishTime readTimes aticleCollect likeNum tags')
 			.sort([['publishTime', -1]])
 			.exec(function(err, aticle){
 				if(err) return handleError(err);
-				data = {
+				res.send({
 					'state': true,
 					'data': aticle
-				}
+				})
 			})
-		res.send(data)
 	})
 module.exports = router
