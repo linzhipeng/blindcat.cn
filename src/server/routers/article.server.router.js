17b70994a7b7c3be89c4f4fb88f52ab725e22344
blindcat <839884316@qq.com>
2017-03-22 20:47:16 +0800

    （后端）增加根据作者id查询文章列表功能

--- src/server/routers/article.server.router.js
+++ src/server/routers/article.server.router.js
@@ -18,31 +18,44 @@ var data
 router
 	.get('/', function(req, res){
 		// 对象解构赋值
+		// searchType  ——查询类型（tags：按标签查，writer：按作者Id查）（默认tags）
 		// pageNum  —— 第几页（默认1）
 		// articleNum  —— 每页规定文章数（默认10）
 		// tags  ——文章标签（all表示全部文章）
-		let {pageNum = 1, articleNum = 10, tags = 'all'} = req.query
+		let {searchType = "tags", pageNum = 1, articleNum = 10, tags = 'all', writerId = ''} = req.query
+		if (!writerId.match(/^[0-9a-fA-F]{24}$/)) {
+			res.send({
+				'state': false,
+				'data': '请输入正确的作者Id'
+			})
+			return false
+		}
 		// 转为整型、正整数
 		pageNum = (parseInt(pageNum) > 0) ? parseInt(pageNum) : 1
 		articleNum = (parseInt(articleNum) > 0) ? parseInt(articleNum) : 10
 
-
-		// 按照标签搜索
-		let searchType = {};
-		if (tags  && tags !== 'all') {
-			searchType = {'tags': {$all: [tags]}}
+		// 搜索query语句
+		let searchQuery = {}
+		// 文章数据
+		let articleListData = {}
+		// 根据searchType生成query语句
+		if (searchType === 'tags') {
+			if (tags  && tags !== 'all') {
+				searchQuery = {'tags': {$all: [tags]}}
+			}
+		} else if (searchType === 'writer') {
+			searchQuery = {'writer': writerId}
 		}
 		
-		let articleListData = {}
 		// 查询符合条件的文章数
-		article.find(searchType).where({auditStatus: 1}).count().exec()
+		article.find(searchQuery).where({auditStatus: 1}).count().exec()
 			.then(data => {
 				if (data > 0) { // 如果文章数大于0
 					// 根据每页文章数计算总页数
 					articleListData.pageCount = Math.ceil(data / articleNum)
 					// 查询符合条件的文章列表
 					return article
-						.find(searchType)
+						.find(searchQuery)
 						.where({auditStatus: 1})
 						.skip((pageNum - 1) * articleNum)
 						.select('_id title abstract publishTime readTimes aticleCollect likeNum tags writer')
